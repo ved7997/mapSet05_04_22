f363f711240fcd9afb951dbdd48cbad9856cb6c2
ved7997 <ved7997@gmail.com>
2022-04-05 21:36:24 +0300

    Initial commit

---
+++ src/main/java/com/example/mapset05_04_22/MapSet050422Application.java
@@ -0,0 +1,13 @@
+package com.example.mapset05_04_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapSet050422Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapSet050422Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mapset05_04_22/MapSet050422ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapset05_04_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapSet050422ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
